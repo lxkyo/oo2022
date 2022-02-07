a5ef65b1033520bfac4747c4ffd1d73dfe2ae9c0
lxkyo <90316779+lxkyo@users.noreply.github.com>
2022-02-07 14:42:37 +0200

    Add files via upload

---
+++ 01_funktsioon/KorrutaKolmega.java
@@ -0,0 +1,11 @@
+public class KorrutaKolmega{
+	public static void main(String[] arg){
+		int arv1 = Integer.parseInt(arg[0]);
+		int arv2 = Integer.parseInt(arg[1]);
+		System.out.println(arv1*arv2);
+		//Koosta programm, mis väljastaks kahe sisestatud arvu korrutise
+		for(int i=0; i<3; i++){
+			System.out.println(arv1);
+		}
+	}
+}
---
+++ 01_funktsioon/Tervitus2.java
@@ -0,0 +1,11 @@
+public class Tervitus2{
+	public static void main(String[]arg){
+		if(arg.length>1){
+			System.out.println("Tere, "+arg[0]+" ja "+arg[1]);
+		} else {
+			System.out.println("Pole kahte nime");
+		}
+		//Kui nimesid on vähemalt kaks, siis tervitage kahte esimest
+		
+	}
+}
